5447abd03966739bf87de5c607b988eec317caf6
hangrybear666 <corey.lauster@gmail.com>
2022-09-24 21:35:57 +0200

    error debug and info logging to cli and file

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,17 +1,22 @@
+const logger = require("../utils/logger")
+
 const errorHandler = (error, request, response, next) => {
 
   // if statusCode is preset, use that, otherwise 500
   const statusCode = response.statusCode ? response.statusCode : 500
+  const errorPojo = {
+    name: error.name,
+    severity: error.severity,
+    code: error.code,
+    routine: error.routine,
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? null : error.stack
+  }
   response
     .status(statusCode)
-    .json({
-      name: error.name,
-      severity: error.severity,
-      code: error.code,
-      routine: error.routine,
-      message: error.message,
-      stack: process.env.NODE_ENV === 'production' ? null : error.stack
-    })
+    .json(errorPojo)
+  logger.error(`{name:"${errorPojo.name}"},{code:"${errorPojo.code}"},{routine:"${errorPojo.routine}"},{message:"${errorPojo.message}"}`)
+  logger.debug(`${errorPojo.stack}`)
   next(error)
 }
 
--- utils/logger.js
+++ utils/logger.js
@@ -1,16 +1,21 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
 
-const logger = winston.createLogger({
+const logger = createLogger({
   level: 'info', // error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
-  format: winston.format.json(),
+  format: format.combine(
+    format.timestamp(),
+    format.align(),
+    format.printf(info => `${info.timestamp ? info.timestamp.trim() : null} ${info.level}: ${info.message ? info.message.trim() : null}`)
+  ),
   // defaultMeta: { service: 'user-service' }, // define own metadata to be added
   transports: [
     //
     // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
+    // - Write all logs with importance level of `debug` or less to `combined.log`
     //
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/info.log', level: 'info' }),
+    new transports.File({ filename: 'logs/debug.log', level: 'debug' })
   ],
 });
 
@@ -19,8 +24,11 @@ const logger = winston.createLogger({
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
 if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
+  logger.add(new transports.Console({
+    format: format.combine(
+      format.colorize(),
+      format.printf(info => `${info.level}: ${info.message ? info.message.trim() : null}`)
+      )
   }));
 }
 
