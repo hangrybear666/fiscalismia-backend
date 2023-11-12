e38ec65807be301548688f1e81c5b5b7dbd3407b
hangrybear666 <corey.lauster@gmail.com>
2023-11-12 14:25:40 +0100

    change of error handler fallback response object

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -8,21 +8,22 @@ const logger = require("../utils/logger")
  * @param {*} next
  */
 const errorHandler = (error, request, response, next) => {
-
   // if statusCode is preset, use that, otherwise 500
   const statusCode = response.statusCode ? response.statusCode : 500
   const errorPojo = {
-    name: error.name,
-    severity: error.severity,
-    code: error.code,
-    routine: error.routine,
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? null : error.stack
+    error : {
+      name: error.name,
+      severity: error.severity,
+      code: error.code,
+      routine: error.routine,
+      message: error.message,
+      stack: process.env.NODE_ENV === 'production' ? null : error.stack
+    },
   }
   // Error Logging to respective log file defined in ../utils/logger
-  logger.error(`{name:"${errorPojo.name}"},{code:"${errorPojo.code}"},{routine:"${errorPojo.routine}"},{message:"${errorPojo.message}"}`)
+  logger.error(`{name:"${errorPojo.error.name}"},{code:"${errorPojo.error.code}"},{routine:"${errorPojo.error.routine}"},{message:"${errorPojo.error.message}"}`)
   // Debug Logging to respective log file defined in ../utils/logger
-  logger.debug(`${errorPojo.stack}`)
+  logger.debug(`${errorPojo.error.stack}`)
 
   // Fallback in case response hasn't been sent by respective method
   response
