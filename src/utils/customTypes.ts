446d5d39d77c2896cf3c8d8c657eb3dbd713a7da
hangrybear666 <hangrybear666+github@proton.me>
2025-09-29 22:03:27 +0200

    Adds PostgresError custom Type

--- src/utils/customTypes.ts
+++ src/utils/customTypes.ts
@@ -161,3 +161,17 @@ export type FoodItem = {
    */
   [key: string]: string | number;
 };
+
+export interface PostgresError extends Error {
+  length: number;
+  name: string;
+  severity: string;
+  code: string;
+  detail: string;
+  schema: string;
+  table: string;
+  constraint: string;
+  file: string;
+  line: string;
+  routine: string;
+}
